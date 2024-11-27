c01f55cefbde5751f301bc0f42df0e852f21fc27
Łukasz Dąbrowski <dabrowskilucas2@gmail.com>
2024-11-27 10:04:08 +0000

    inital commit

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,16 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new())
+}
+#[ic_cdk::update]
+fn set_msg(msg: String) {
+    MSG.with(|static_msg| *static_msg.borrow_mut() = msg )
+}
+#[ic_cdk::query]
+fn get_msg() -> String  {
+    MSG.with(|static_msg| static_msg.borrow().clone() )
+}
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
